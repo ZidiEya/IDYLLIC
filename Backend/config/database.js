7ff51e40cb38c77b7b2e8a33a44198017586084f
ZidiEya <3805@holbetonschool.com>
2022-06-24 16:43:16 -0700

    database configuration

---
+++ Backend/config/database.js
@@ -0,0 +1,20 @@
+const {  connect } =require('mongoose');
+const { success,error} = require('consola');
+
+const DB = process.env.APP_DB;
+const connectDB = async () =>{
+    try{
+	await connect(DB);
+	success({
+	    message: `succssfully connected with database \n ${DB}`,
+	    badge:true,
+	});
+    }catch(err){
+	error({
+	    message: `unable to connected with database \n ${err}`,
+	    badge:true,
+	});
+	connectDB();
+    }
+};
+module.exports= connectDB();
