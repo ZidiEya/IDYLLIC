aac1602bc0f11fe79d17f42bf241287b59559d83
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2022-07-07 03:41:12 +0100

    Update register.component.ts

--- frontend/register/register.component.ts
+++ frontend/register/register.component.ts
@@ -1,3 +1,4 @@
+// register component
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
