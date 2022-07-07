5f7e3781d4f6bc36c0eb8309877eef95b9bcf314
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2022-07-07 02:37:39 +0100

    Update Admin.js

--- Backend/Models/Admin.js
+++ Backend/Models/Admin.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 const User=require('./User')
 const adminSchema=new mongoose.Schema()
-
+// admin model
 module.exports=User.discriminator('Admin',adminSchema)
 
 
