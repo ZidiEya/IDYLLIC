f8f36d65a6b35d695d5445f34a41f5769e1f4efd
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2022-07-07 03:39:02 +0100

    Update profil.component.ts

--- frontend/profil/profil.component.ts
+++ frontend/profil/profil.component.ts
@@ -1,3 +1,4 @@
+// profile component
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
