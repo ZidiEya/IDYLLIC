16b38ab70cc0817418f6782ccf01b38d00761503
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2022-07-07 03:18:17 +0100

    Update detailproduct.component.ts

--- frontend/detailproduct/detailproduct.component.ts
+++ frontend/detailproduct/detailproduct.component.ts
@@ -1,3 +1,4 @@
+// detail products component
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../product.service';
